5645a23aa5fae6f36bb754825ee4931771bfa64d
levi <levi.scott0629@gmail.com>
2025-12-17 12:17:52 -0500

    created enum in degree.h

---
+++ degree.h
@@ -0,0 +1,23 @@
+#ifndef DEGREE_H
+#define DEGREE_H
+
+#include <string>
+
+enum DegreeProgram {
+    SECURITY,
+    NETWORK,
+    SOFTWARE
+};
+
+inline std::string DegreeProgramToString(DegreeProgram dp) {
+    switch (dp) {
+    case SECURITY:
+        return "Security";
+    case NETWORK:
+        return "Network";
+    case SOFTWARE:
+        return "Software";
+    }
+}
+
+#endif // DEGREE_H
